c8472ac7d667b4d22c503820615ccf4fd3f09a0c
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-08-29 10:18:11 -0700

    Initial Commit

---
+++ iOS Tip App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Tip App
+//
+//  Created by Samuel Sandoval on 8/29/20.
+//  Copyright © 2020 Samuel Sandoval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
