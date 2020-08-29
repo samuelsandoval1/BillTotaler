66a4bb009905937047444059932c3d0726fd8da0
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-08-29 14:26:38 -0700

    Added outlets, gesture feature, and setup ViewController

--- iOS Tip App/ViewController.swift
+++ iOS Tip App/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    
+    @IBOutlet weak var tipAmountSegmnetedControl: UISegmentedControl!
+    
+     @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    
 }
 
